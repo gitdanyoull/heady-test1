bee13c4b5b5b10eb467a8cc72eacfcec64408259
Stephane Martin-Dupont <smartindupont@gmail.com>
2017-03-09 02:32:08 +0800

    add form event listener to select wich field to submit to the user

--- src/AppBundle/Form/PostFormType.php
+++ src/AppBundle/Form/PostFormType.php
@@ -14,6 +14,8 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
@@ -27,32 +29,46 @@ class PostFormType extends AbstractType
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
-    { 
-        
+    {
         $builder
             ->add('title', TextType::class)
             ->add('price', TextType::class)
             ->add('artist', EntityType::class, [
                 // This wil be change depending on the value
                 'mapped' => false,
+                'required' => false,
                 'property_path' => 'artist',
                 'empty_value'=>'Choose Artist',
                 'class'=>Artist::class,
                 'choice_label'=>'artist',
                 'query_builder'=> function (EntityRepository $er) {
-                    return $er->createQueryBuilder('artist')
-                        ->orderBy('artist.artist', 'ASC');
+                    $list_artist = $er->createQueryBuilder('artist')
+                        ->orderBy('artist.artist', 'ASC')
+                    ;
+                    return $list_artist;
                 },
             ])
             // For now just display the embed form, will be hidden later
             ->add('artist_form', ArtistType::class, array(
                 // This wil be change depending on the value
                 'mapped' => false,
+                'required' => false,
                 'property_path' => 'artist',
             ))
             ->add('description', TextareaType::class)
             ->add('purchaseDate','date')
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $user_data = $event->getData();
+            $form = $event->getForm();
+            $artist_field = empty($user_data['artist'])? 'artist_form' : 'artist';
+            $config = $form->get($artist_field)->getConfig();
+            $options = $config->getOptions();
+            $options['mapped'] = true;
+            $options['required'] = true;
+            $form->add($artist_field, $config->getType()->getName(), $options);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
