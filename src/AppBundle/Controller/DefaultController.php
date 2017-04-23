67840cbf726987219f2eb84e463ad66449f86d34
Stephane Martin-Dupont <smartindupont@gmail.com>
2017-04-23 11:47:11 +0800

    update default controller (no need to query manually images doctrine will do it when accessing the images property of the post)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,21 +14,11 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $post_repository = $this->getDoctrine()->getRepository('AppBundle:Post');
-        $post= $post_repository->findAll();
-        
-        $img_repository = $this->getDoctrine()->getRepository('AppBundle:Image');
-        $row = 0;
-        $glass = array();
-        foreach( $post as $k => $v ){
-            $images= $img_repository->findBy( array('postId' => $v->getId() ));
-            $glass[$row]['post']=$v;
-            $glass[$row]['images']=$images;
-            ++$row;
-        }
+        $posts = $post_repository->findAll();
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'posts'=>$glass,
+            'posts' => $posts,
         ));
     }
 }
